0405c559fe8cd9a133bc6417795c0fd4d3483e9a
Marlon Richert <marlon@vaadin.com>
2016-03-17 15:42:15 +0200

    Remove widgetset annotation

--- login-dialog/src/main/java/org/vaadin/example/logindialog/LoginUI.java
+++ login-dialog/src/main/java/org/vaadin/example/logindialog/LoginUI.java
@@ -4,7 +4,6 @@ import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
-import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
@@ -13,11 +12,7 @@ import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
-/**
- *
- */
 @Theme("login-dialog")
-@Widgetset("org.vaadin.example.logindialog.LoginDialogWidgetset")
 public class LoginUI extends UI {
 
 	@Override
--- payment-dialog/src/main/java/org/vaadin/example/paymentdialog/UI.java
+++ payment-dialog/src/main/java/org/vaadin/example/paymentdialog/UI.java
@@ -4,13 +4,11 @@ import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
-import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 
 @SuppressWarnings("serial")
 @Theme("payment-dialog")
-@Widgetset("org.vaadin.example.paymentdialog.PaymentDialogWidgetset")
 public class UI extends com.vaadin.ui.UI {
 	@WebServlet(value = "/*", asyncSupported = true)
 	@VaadinServletConfiguration(productionMode = false, ui = UI.class)
