fedc48f5e1bded265e6c2e3aa0b8f1a9191440e8
Marlon Richert <marlon@vaadin.com>
2016-03-17 13:05:04 +0200

    Migrate from Ivy to Maven

--- payment-dialog/src/main/java/org/vaadin/example/paymentdialog/UI.java
+++ payment-dialog/src/main/java/org/vaadin/example/paymentdialog/UI.java
@@ -4,14 +4,16 @@ import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 
 @SuppressWarnings("serial")
 @Theme("payment-dialog")
+@Widgetset("org.vaadin.example.paymentdialog.PaymentDialogWidgetset")
 public class UI extends com.vaadin.ui.UI {
 	@WebServlet(value = "/*", asyncSupported = true)
-	@VaadinServletConfiguration(productionMode = false, ui = UI.class, widgetset = "org.vaadin.example.paymentdialog.widgetset.PaymentDialogWidgetset")
+	@VaadinServletConfiguration(productionMode = false, ui = UI.class)
 	public static class Servlet extends VaadinServlet {
 	}
 
