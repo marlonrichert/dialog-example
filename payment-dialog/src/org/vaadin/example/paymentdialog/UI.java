2eed80d2759749a005f080cd76a23bb6337e24c8
Marlon Richert <marlon@vaadin.com>
2016-03-17 12:14:05 +0200

    Rename DialogExample to UI

--- payment-dialog/src/org/vaadin/example/paymentdialog/UI.java
+++ payment-dialog/src/org/vaadin/example/paymentdialog/UI.java
@@ -6,13 +6,12 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.UI;
 
 @SuppressWarnings("serial")
 @Theme("dialog-example")
-public class DialogExample extends UI {
+public class UI extends com.vaadin.ui.UI {
 	@WebServlet(value = "/*", asyncSupported = true)
-	@VaadinServletConfiguration(productionMode = false, ui = DialogExample.class, widgetset = "com.example.dialog.widgetset.Dialog_exampleWidgetset")
+	@VaadinServletConfiguration(productionMode = false, ui = UI.class, widgetset = "com.example.dialog.widgetset.Dialog_exampleWidgetset")
 	public static class Servlet extends VaadinServlet {
 	}
 
